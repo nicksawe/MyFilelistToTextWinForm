cc9d1e9859816cd568c2c0b18cd6cb40b5b0a6c1
Nick Sawe <nicksawe@users.noreply.github.com>
2015-08-14 12:29:19 +0100

    Now using list instead of IEnumrable

--- MyFilelistToTextWinForm/Form1.cs
+++ MyFilelistToTextWinForm/Form1.cs
@@ -7,10 +7,9 @@ using System.Windows.Forms;
 namespace MyFilelistToTextWinForm
 {
     public partial class Form1 : Form
-    {                                                               
-            static string path = @"w:\Videos\Movies\";                     
-            static string pathToWriteTo = @"c:\opt\mymovies.txt";          
-
+    {
+        private static readonly string path = @"w:\Videos\Movies\";
+        private static readonly string pathToWriteTo = @"c:\opt\mymovies.txt";
 
         public Form1()
         {
@@ -19,23 +18,16 @@ namespace MyFilelistToTextWinForm
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-
-           // string[] files = Directory.GetFiles(path); - works
-
-          //  List<string> filelist = new List<string>(); - not tested
-
+            // string[] files = Directory.GetFiles(path); - works
 
             // comm var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
 
-            IEnumerable<string> files;
+            List<string> files;
             int filecount;
             DateTime start;
             DateTime end;
 
-            GetFileData(out files,out filecount, out start,out end);
-
-          //  var result = files.OrderBy(s => s);//sorted alphabetically ! SO tip
+            GetFileData(out files, out filecount, out start, out end);
 
             using (var x = File.CreateText(pathToWriteTo))
             {
@@ -45,22 +37,21 @@ namespace MyFilelistToTextWinForm
                 }
             }
 
-            
-            lblResult.Text = filecount.ToString() + @" files found in " + (end - start).TotalSeconds;
+
+            lblResult.Text = filecount + @" files found in " + (end - start).TotalSeconds;
         }
 
-       
-
-        private void GetFileData(out IEnumerable<string> files, out int filecount, out DateTime start, out DateTime end)
+        private void GetFileData(out List<string> files, out int filecount, out DateTime start, out DateTime end)
         {
             start = DateTime.Now;
 
-            files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+            var temp = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories).ToList();
+            temp.Sort();
+            files = temp;
+
             filecount = files.Count();
 
             end = DateTime.Now;
         }
-
-
     }
 }
